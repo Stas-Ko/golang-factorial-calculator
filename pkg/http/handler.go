dd636f17751912a4e2e2514f39475d6bbe55e4a3
Stas-Ko <142270795+Stas-Ko@users.noreply.github.com>
2023-11-28 19:10:24 +0200

    Update handler.go

--- pkg/http/handler.go
+++ pkg/http/handler.go
@@ -1,1 +1,25 @@
+// pkg/http/handler.go
+package http
+
+import (
+	"encoding/json"
+	"net/http"
+	"github.com/julienschmidt/httprouter"
+	"github.com/Stas-Ko/golang-factorial-calculator/pkg/calculate"
+)
+
+// Result представляє структуру для JSON-відповіді.
+type Result struct {
+	AFactorial      string `json:"a_factorial"`
+	BFactorial      string `json:"b_factorial"`
+	AFactorialValue int    `json:"a_factorial_value"`
+	BFactorialValue int    `json:"b_factorial_value"`
+}
+
+// CalculateHandler обробляє запит для обчислення факторіала.
+func CalculateHandler(calculator *calculate.Calculator) httprouter.Handle {
+	return func(w http.ResponseWriter, r *http.Request, params httprouter.Params) {
+		// ... ваш код для обробки запиту
+	}
+}
 
