631d89ba38a7ddea464e23aeaac04e76a14ce3b2
Stas-Ko <142270795+Stas-Ko@users.noreply.github.com>
2023-11-28 19:11:32 +0200

    Update handler_test.go

--- pkg/http/handler_test.go
+++ pkg/http/handler_test.go
@@ -1,1 +1,15 @@
+// pkg/http/handler_test.go
+package http
+
+import (
+	"testing"
+	"net/http"
+	"net/http/httptest"
+	"github.com/julienschmidt/httprouter"
+	"github.com/Stas-Ko/golang-factorial-calculator/pkg/calculate"
+)
+
+func TestCalculateHandler(t *testing.T) {
+	// ... ваші тести для обробника
+}
 
