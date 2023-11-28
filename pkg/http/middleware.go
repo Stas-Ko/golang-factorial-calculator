2398d73bff1520fec94ddcc51775f700d83053fe
Stas-Ko <142270795+Stas-Ko@users.noreply.github.com>
2023-11-28 19:17:35 +0200

    Update middleware.go

--- pkg/http/middleware.go
+++ pkg/http/middleware.go
@@ -2,14 +2,14 @@
 package http
 
 import (
-	"encoding/json"
 	"net/http"
+	"encoding/json"
 	"github.com/julienschmidt/httprouter"
 	"github.com/Stas-Ko/golang-factorial-calculator/pkg/calculate"
 )
 
 // Middleware перевіряє, чи a і b є невід'ємними цілими числами в JSON-запиті.
-func Middleware(next httprouter.Handle) httprouter.Handle {
+func Middleware(next httprouter.Handle, calculator *calculate.Calculator) httprouter.Handle {
 	return func(w http.ResponseWriter, r *http.Request, params httprouter.Params) {
 		// ... ваш код middleware
 	}
