227c28af56e59078098dc748a708f09c37e60c7e
Stas-Ko <142270795+Stas-Ko@users.noreply.github.com>
2023-11-28 18:53:28 +0200

    Update main.go

--- main.go
+++ main.go
@@ -1,11 +1,26 @@
+// main.go
 package main
 
 import (
 	"fmt"
+	"github.com/julienschmidt/httprouter"
 	"github.com/Stas-Ko/golang-factorial-calculator/pkg/http"
+	"github.com/Stas-Ko/golang-factorial-calculator/pkg/calculate"
 )
 
+// ...
+
+// CalculateHandler обрабатывает запрос для расчета факториала.
+func calculateHandler(w http.ResponseWriter, r *http.Request, _ httprouter.Params, calculator calculate.Calculator) {
+	// ...
+}
+
+// ...
+
 func main() {
-	router := http.NewRouter()
+	router := http.NewRouter(calculator)
+	router.POST("/calculate", calculateHandler)
+
+	fmt.Println("Server is running on port 8989...")
 	http.StartServer(router)
 }
