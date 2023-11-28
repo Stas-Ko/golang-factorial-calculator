86d60c717babe30bd1e2753a874d6cdf16b5ddac
Stas-Ko <142270795+Stas-Ko@users.noreply.github.com>
2023-11-28 18:05:51 +0200

    Update server.go

--- pkg/http/server.go
+++ pkg/http/server.go
@@ -1,19 +1,22 @@
 package http
 
 import (
-    "fmt"
-    "github.com/julienschmidt/httprouter"
+	"fmt"
+	"net/http"
+	"sync"
+
+	"github.com/julienschmidt/httprouter"
 )
 
 // NewRouter создает новый роутер.
 func NewRouter() *httprouter.Router {
-    router := httprouter.New()
-    router.POST("/calculate", Middleware(CalculateHandler))
-    return router
+	router := httprouter.New()
+	router.POST("/calculate", Middleware(CalculateHandler))
+	return router
 }
 
 // StartServer запускает сервер.
 func StartServer(router *httprouter.Router) {
-    fmt.Println("Server is running on port 8989...")
-    http.ListenAndServe(":8989", router)
+	fmt.Println("Server is running on port 8989...")
+	http.ListenAndServe(":8989", router)
 }
