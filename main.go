f6c2bad9da915000300ed30f6fed5627956bd799
Stas-Ko <142270795+Stas-Ko@users.noreply.github.com>
2023-11-28 20:45:49 +0200

    Update main.go

--- main.go
+++ main.go
@@ -2,8 +2,8 @@ package main
 
 import (
 	"fmt"
-	"github.com/Stas-Ko/golang-factorial-calculator/blob/main/pkg/http/handler.go"
-	"github.com/julienschmidt/httprouter"
+	"github.com/Stas-Ko/golang-factorial-calculator/pkg/http"
+	
 )
 
 func main() {
