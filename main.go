da530733d912671fbc028e80598d8b1ee8e9285e
Stas-Ko <142270795+Stas-Ko@users.noreply.github.com>
2023-11-28 18:40:58 +0200

    Update main.go

--- main.go
+++ main.go
@@ -2,18 +2,10 @@ package main
 
 import (
 	"fmt"
-	"github.com/julienschmidt/httprouter"
-	"net/http"
+	"github.com/Stas-Ko/golang-factorial-calculator/pkg/http"
 )
 
 func main() {
-	router := httprouter.New()
-	router.POST("/calculate", CalculateHandler)
-
-	fmt.Println("Server is running on port 8989...")
-	http.ListenAndServe(":8989", router)
-}
-
-func CalculateHandler(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
-	fmt.Fprintln(w, "Hello, this is your calculate endpoint!")
+	router := http.NewRouter()
+	http.StartServer(router)
 }
