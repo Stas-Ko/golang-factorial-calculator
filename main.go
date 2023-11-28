8a585c05953f8fb8c5226e381d1ba52b7392cf2c
Stas-Ko <142270795+Stas-Ko@users.noreply.github.com>
2023-11-28 21:08:41 +0200

    Update main.go

--- main.go
+++ main.go
@@ -2,13 +2,15 @@ package main
 
 import (
 	"fmt"
-	"github.com/Stas-Ko/golang-factorial-calculator/pkg/http"
-	
+	"github.com/your-username/golang-factorial-calculator/pkg/http"
 )
 
 func main() {
-	server := http.NewServer(8989)
-	fmt.Println("Server is running on port 8989...")
-	server.Start()
-}
+	server := http.NewServer()
 
+	fmt.Println("Server is running on port 8989...")
+	err := server.ListenAndServe()
+	if err != nil {
+		fmt.Printf("Error starting server: %s\n", err)
+	}
+}
