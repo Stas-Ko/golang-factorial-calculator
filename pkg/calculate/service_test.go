e33190bf0b97966d5e25ac579dabee92c5887b8f
Stas-Ko <142270795+Stas-Ko@users.noreply.github.com>
2023-11-28 16:37:01 +0200

    Update service_test.go

--- pkg/calculate/service_test.go
+++ pkg/calculate/service_test.go
@@ -1,1 +1,10 @@
+package calculate
+
+import "testing"
+
+func TestCalculateFactorial(t *testing.T) {
+	// ... ваш тест для calculateFactorial
+}
+
+// Аналогично, добавьте тесты для других функций в пакете calculate.
 
