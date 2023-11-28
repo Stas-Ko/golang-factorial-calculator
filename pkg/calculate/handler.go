02d6c4787c8b7630bf3b84f6bcebd7363212532d
Stas-Ko <142270795+Stas-Ko@users.noreply.github.com>
2023-11-28 18:13:05 +0200

    Update handler.go

--- pkg/calculate/handler.go
+++ pkg/calculate/handler.go
@@ -1,10 +1,13 @@
 package calculate
 
 import (
-    "encoding/json"
-    "net/http"
-    "sync"
+	"encoding/json"
+	"net/http"
+	"sync"
+
+	"github.com/julienschmidt/httprouter"
 )
+
 
 // CalculateHandler обрабатывает запрос для расчета факториала.
 func CalculateHandler(w http.ResponseWriter, r *http.Request, params httprouter.Params) {
