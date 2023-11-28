3740a8c96fc48eb17ec44822f1f524f40a579f95
Stas-Ko <142270795+Stas-Ko@users.noreply.github.com>
2023-11-28 18:44:31 +0200

    Update middleware.go

--- pkg/calculate/middleware.go
+++ pkg/calculate/middleware.go
@@ -1,12 +1,22 @@
 package calculate
 
 import (
+	"encoding/json"
 	"net/http"
+
+	"github.com/julienschmidt/httprouter"
 )
 
-// Middleware просто пропускает запрос дальше.
+// Middleware проверяет, что a и b являются неотрицательными целыми числами в JSON-запросе.
 func Middleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		decoder := json.NewDecoder(r.Body)
+		var inputData InputData
+		err := decoder.Decode(&inputData)
+		if err != nil || inputData.A < 0 || inputData.B < 0 {
+			http.Error(w, "Incorrect input", http.StatusBadRequest)
+			return
+		}
 		next.ServeHTTP(w, r)
 	})
 }
