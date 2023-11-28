75004c3706e572dd53785ca3ec7bdd3e2413809c
Stas-Ko <142270795+Stas-Ko@users.noreply.github.com>
2023-11-28 16:44:44 +0200

    Update server_test.go

--- pkg/http/server_test.go
+++ pkg/http/server_test.go
@@ -3,12 +3,12 @@ package http
 import "testing"
 
 func TestNewRouter(t *testing.T) {
-	// ... ваш тест для NewRouter
+	// ... тест для NewRouter
 }
 
 func TestStartServer(t *testing.T) {
-	// ... ваш тест для StartServer
+	// ... тест для StartServer
 }
 
-// Аналогично, добавьте тесты для других функций и методов в пакете http.
+
 
