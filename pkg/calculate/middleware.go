5767e362aae34179088ee566c272a5a44fc23b46
Stas-Ko <142270795+Stas-Ko@users.noreply.github.com>
2023-11-28 18:19:45 +0200

    Update middleware.go

--- pkg/calculate/middleware.go
+++ pkg/calculate/middleware.go
@@ -4,21 +4,25 @@ import (
 	"encoding/json"
 	"net/http"
 	"sync"
-
-	"github.com/julienschmidt/httprouter"
 )
 
-// Middleware проверяет, что a и b являются неотрицательными целыми числами в JSON-запросе.
+// InputData - структура для представления данных в запросе.
+type InputData struct {
+	A int `json:"a"`
+	B int `json:"b"`
+}
+
+// Middleware проверяет, что A и B являются неотрицательными целыми числами в JSON-запросе.
 func Middleware(next http.Handler) http.Handler {
-    return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-        decoder := json.NewDecoder(r.Body)
-        var inputData InputData
-        err := decoder.Decode(&inputData)
-        if err != nil || inputData.A < 0 || inputData.B < 0 {
-            http.Error(w, "Incorrect input", http.StatusBadRequest)
-            return
-        }
-        next.ServeHTTP(w, r)
-    })
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		decoder := json.NewDecoder(r.Body)
+		var inputData InputData
+		err := decoder.Decode(&inputData)
+		if err != nil || inputData.A < 0 || inputData.B < 0 {
+			http.Error(w, "Incorrect input", http.StatusBadRequest)
+			return
+		}
+		next.ServeHTTP(w, r)
+	})
 }
 
