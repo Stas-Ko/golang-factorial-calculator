6fc90e5749eaf8a386d3c2ff3eb918bdf2198960
Stas-Ko <142270795+Stas-Ko@users.noreply.github.com>
2023-11-28 18:45:49 +0200

    Update middleware_test.go

--- pkg/calculate/middleware_test.go
+++ pkg/calculate/middleware_test.go
@@ -1,12 +1,7 @@
 package calculate
 
-import (
-    "net/http"
-    "net/http/httptest"
-    "testing"
-)
+import "testing"
 
 func TestMiddleware(t *testing.T) {
-    // ... ваш тест для Middleware
+	// Ваши тесты для Middleware
 }
-
