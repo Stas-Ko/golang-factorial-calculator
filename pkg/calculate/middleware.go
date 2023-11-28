2de2926b3501f334c56ca118e8bf4a6ef8f64dfd
Stas-Ko <142270795+Stas-Ko@users.noreply.github.com>
2023-11-28 18:24:16 +0200

    Update middleware.go

--- pkg/calculate/middleware.go
+++ pkg/calculate/middleware.go
@@ -1,28 +1,12 @@
 package calculate
 
 import (
-	"encoding/json"
 	"net/http"
-	"sync"
 )
 
-// InputData - структура для представления данных в запросе.
-type InputData struct {
-	A int `json:"a"`
-	B int `json:"b"`
-}
-
-// Middleware проверяет, что A и B являются неотрицательными целыми числами в JSON-запросе.
+// Middleware просто пропускает запрос дальше.
 func Middleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		decoder := json.NewDecoder(r.Body)
-		var inputData InputData
-		err := decoder.Decode(&inputData)
-		if err != nil || inputData.A < 0 || inputData.B < 0 {
-			http.Error(w, "Incorrect input", http.StatusBadRequest)
-			return
-		}
 		next.ServeHTTP(w, r)
 	})
 }
-
