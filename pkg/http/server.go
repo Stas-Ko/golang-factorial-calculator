161865d3f9e01e4a8a5470b3431b07be1aa1a7b0
Stas-Ko <142270795+Stas-Ko@users.noreply.github.com>
2023-11-28 18:25:09 +0200

    Update server.go

--- pkg/http/server.go
+++ pkg/http/server.go
@@ -2,22 +2,23 @@ package http
 
 import (
 	"fmt"
-	"net/http"
-	"sync"
-
 	"github.com/julienschmidt/httprouter"
 	"github.com/Stas-Ko/golang-factorial-calculator/pkg/calculate"
+	"net/http"
 )
 
-// NewRouter создает новый роутер.
 func NewRouter() *httprouter.Router {
 	router := httprouter.New()
-	router.POST("/calculate", Middleware(calculate.CalculateHandler))
+	router.POST("/calculate", calculate.Middleware(CalculateHandler))
 	return router
 }
 
-// StartServer запускает сервер.
 func StartServer(router *httprouter.Router) {
 	fmt.Println("Server is running on port 8989...")
 	http.ListenAndServe(":8989", router)
 }
+
+func CalculateHandler(w http.ResponseWriter, r *http.Request, params httprouter.Params) {
+	fmt.Fprintln(w, "Hello, this is your calculate handler!")
+}
+
