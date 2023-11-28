b3ee818961122276008bae53c824b05e66f8c01f
Stas-Ko <142270795+Stas-Ko@users.noreply.github.com>
2023-11-28 17:33:20 +0200

    Update service.go

--- pkg/calculate/service.go
+++ pkg/calculate/service.go
@@ -1,9 +1,9 @@
 package calculate
 
+// CalculateFactorial рассчитывает факториал.
 func CalculateFactorial(n int) int {
-	if n == 0 || n == 1 {
-		return 1
-	}
-	return n * CalculateFactorial(n-1)
+    if n == 0 || n == 1 {
+        return 1
+    }
+    return n * CalculateFactorial(n-1)
 }
-
